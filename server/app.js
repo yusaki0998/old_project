cf3f3a3669864a6a13335cf735debcc40548c698
tutv3 <tutv3@smartosc.com>
2021-06-21 22:44:42 +0700

    update cors

--- server/app.js
+++ server/app.js
@@ -4,15 +4,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 //const db = require('./config/db');
 const morgan = require("morgan");
+const path = require("path");
 
 //Declare routes
-const userRoutes = require('./routes/user-route');
-const movieRoutes = require('./routes/movie-route');
-const roomRoutes = require('./routes/room-route');
-const slotRoutes = require('./routes/slot-route');
+const userRoutes = require("./routes/user-route");
+const movieRoutes = require("./routes/movie-route");
+const roomRoutes = require("./routes/room-route");
+const slotRoutes = require("./routes/slot-route");
 
 //Database connect
 const uri =
@@ -40,6 +41,7 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // cors middleware
 app.use((req, res, next) => {
@@ -59,10 +61,10 @@ app.get("/", (req, res) => {
 });
 
 //routes
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/movies', movieRoutes);
-app.use('/api/v1/rooms', roomRoutes);
-app.use('/api/v1/slots', slotRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/movies", movieRoutes);
+app.use("/api/v1/rooms", roomRoutes);
+app.use("/api/v1/slots", slotRoutes);
 
 //error handling
 app.use((req, res, next) => {
