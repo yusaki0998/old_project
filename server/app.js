ccc465053025a8da3b14efbd129f5a719873acdd
ridotoki <40660258+ridotoki@users.noreply.github.com>
2021-07-16 11:21:37 +0700

    small update

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 //const mongoose = require("mongoose");
 const cors = require('cors');
-const db = require('./config/db');
+require('./config/db');
 const morgan = require("morgan");
 
 //Declare routes
